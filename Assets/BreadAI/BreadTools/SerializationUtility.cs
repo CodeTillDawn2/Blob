069d3da92c0e9fe3feb31f2ad25fab33d5af6729
CodeTillDawn2 <139724053+CodeTillDawn2@users.noreply.github.com>
2023-08-12 10:48:51 -0400

    Didn't like the serialization but I am keeping the ui tools

--- Assets/BreadAI/BreadTools/SerializationUtility.cs
+++ Assets/BreadAI/BreadTools/SerializationUtility.cs
@@ -1,7 +1,13 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+using System.Collections;
+using System;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.Assertions;
+using System.IO;
+using System.Linq;
 
 public static class SerializationUtility
 {
@@ -35,5 +41,103 @@ public static class SerializationUtility
             Formatting = Formatting.Indented
         };
         return JsonConvert.SerializeObject(obj, settings);
+    }
+
+    /// <summary>
+    /// This is a list of all known configs which can be serialized. Allows me not to have to test check every time 
+    /// since I know there are good configurations
+    /// </summary>
+    private static HashSet<Type> GoodConfigs = new HashSet<Type>() { typeof(Dictionary<string, string>),
+                                    typeof(Dictionary<String, Dictionary<String, List<String>>>)};
+
+    public static void WriteToDisk<T>(T dataObject, string filePath)
+    {
+        try
+        {
+            // Use the new SerializationUtility to get the JSON string.
+            string json = SerializationUtility.SerializeObject(dataObject);
+            File.WriteAllText(filePath, json);
+
+            if (!GoodConfigs.Contains(typeof(T)))
+            {
+                string testingJson = File.ReadAllText(filePath);
+                T testingObject = JsonConvert.DeserializeObject<T>(json);
+                Assert.AreEqual(dataObject, testingObject);
+                Debug.LogWarning($"Type successfully tested. Please add 'typeof({GetFriendlyTypeName(typeof(T))})' to GoodConfigs. {Environment.StackTrace}");
+            }
+        }
+        catch (AssertionException ex)
+        {
+            Debug.LogError($"Serialization failed, too complex of an object. {ex.Message}. Initiating detailed check. Object signature:  '{GetFriendlyTypeName(typeof(T))}' Stack Trace: {Environment.StackTrace}");
+            TestSerializabilityOfObject(dataObject);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"Serialization encountered issues for type '{GetFriendlyTypeName(typeof(T))}'. Initiating detailed check. Reason for error: {ex.Message} Stack Trace: {Environment.StackTrace}");
+            TestSerializabilityOfObject(dataObject);
+        }
+    }
+
+    private static void TestSerializabilityOfObject<T>(T obj, string parentPath = "", int depth = 0)
+    {
+        const int MAX_DEPTH = 10; // Change this value based on what's reasonable for your structures.
+
+        if (depth > MAX_DEPTH)
+        {
+            Debug.LogWarning($"Reached maximum depth at path: {parentPath}. Exiting...");
+            return;
+        }
+
+        if (obj == null) return;
+
+        if (obj is IDictionary)
+        {
+            IDictionary dictionary = obj as IDictionary;
+            foreach (DictionaryEntry entry in dictionary)
+            {
+                TestSerializabilityOfObject(entry.Value, $"{parentPath}/{entry.Key}", depth + 1);
+            }
+        }
+        else if (obj is IList)
+        {
+            IList list = obj as IList;
+            for (int i = 0; i < list.Count; i++)
+            {
+                TestSerializabilityOfObject(list[i], $"{parentPath}/[{i}]", depth + 1);
+            }
+        }
+        else if (obj is IEnumerable && !(obj is string))
+        {
+            // Handle other enumerable types like arrays or custom collections
+            int i = 0;
+            foreach (var item in (IEnumerable)obj)
+            {
+                TestSerializabilityOfObject(item, $"{parentPath}/[{i}]", depth + 1);
+                i++;
+            }
+        }
+
+        try
+        {
+            // Use the new SerializationUtility to get the JSON string.
+            string json = SerializationUtility.SerializeObject(obj);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"Serialization failed for path '{parentPath}' in type '{GetFriendlyTypeName(typeof(T))}'. Reason: {ex.Message}");
+        }
+    }
+
+
+
+
+    public static string GetFriendlyTypeName(Type t)
+    {
+        if (!t.IsGenericType)
+            return t.Name;
+
+        string name = t.Name.Substring(0, t.Name.IndexOf('`'));
+        string typeParameters = string.Join(", ", t.GetGenericArguments().Select(GetFriendlyTypeName).ToArray());
+        return $"{name}<{typeParameters}>";
     }
 }
--- Assets/Editor/BreadAI/AIEditorBaker.cs
+++ Assets/Editor/BreadAI/AIEditorBaker.cs
@@ -81,9 +81,9 @@ public static class AIEditorBaker
         isFirstLog = true;
         BakeConfigurationAssignmentLogic();
         AllConfigInstances = GetAllInstancesOfDerived<ConfigurationBase>(); //Must happen before BakeCharacterSystemsToConfigMappings
-        WriteToDisk<Dictionary<string, ScriptableObject>>(AllConfigInstances, AllConfigInstances_Path);
+        SerializationUtility.WriteToDisk<Dictionary<string, ScriptableObject>>(AllConfigInstances, AllConfigInstances_Path);
         CharacterSystemToConfigMapping = BakeCharacterSystemsToConfigMappings();
-        WriteToDisk<Dictionary<string, Dictionary<string, List<string>>>>(CharacterSystemToConfigMapping, CharacterSystemToConfigMapping_Path);
+        SerializationUtility.WriteToDisk<Dictionary<string, Dictionary<string, List<string>>>>(CharacterSystemToConfigMapping, CharacterSystemToConfigMapping_Path);
         ConfigurationInstances = BakeConfigurationInstances();
         AIAttributesCache = BakeAttributesInCharacterSystems();
         ScriptableObjectPropertiesDetection = BakeScriptableObjectPropertiesDetection();
@@ -92,117 +92,7 @@ public static class AIEditorBaker
     public static string CharacterSystemToConfigMapping_Path = Application.dataPath + @"/BreadAI/BreadBake/BakedData/CharacterSystemToConfigMapping.json";
     public static string AllConfigInstances_Path = Application.dataPath + @"/BreadAI/BreadBake/BakedData/AllConfigInstances.json";
 
-    /// <summary>
-    /// This is a list of all known configs which can be serialized. Allows me not to have to test check every time 
-    /// since I know there are good configurations
-    /// </summary>
-    private static HashSet<Type> GoodConfigs = new HashSet<Type>() { typeof(Dictionary<string, string>),
-                                    typeof(Dictionary<String, Dictionary<String, List<String>>>)};
-
-    public static void WriteToDisk<T>(T dataObject, string filePath)
-    {
-        try
-        {
-            // Use the new SerializationUtility to get the JSON string.
-            string json = SerializationUtility.SerializeObject(dataObject);
-            File.WriteAllText(filePath, json);
-
-            if (!GoodConfigs.Contains(typeof(T)))
-            {
-                string testingJson = File.ReadAllText(filePath);
-                T testingObject = JsonConvert.DeserializeObject<T>(json);
-                Assert.AreEqual(dataObject, testingObject);
-                Debug.LogWarning($"Type successfully tested. Please add 'typeof({GetFriendlyTypeName(typeof(T))})' to GoodConfigs. {Environment.StackTrace}");
-            }
-        }
-        catch (AssertionException ex)
-        {
-            Debug.LogError($"Serialization failed, too complex of an object. {ex.Message}. Initiating detailed check. Object signature:  '{GetFriendlyTypeName(typeof(T))}' Stack Trace: {Environment.StackTrace}");
-            TestSerializabilityOfObject(dataObject);
-        }
-        catch (Exception ex)
-        {
-            Debug.LogError($"Serialization encountered issues for type '{GetFriendlyTypeName(typeof(T))}'. Initiating detailed check. Reason for error: {ex.Message} Stack Trace: {Environment.StackTrace}");
-            TestSerializabilityOfObject(dataObject);
-        }
-    }
-
-    private static void TestSerializabilityOfObject<T>(T obj, string parentPath = "", int depth = 0)
-    {
-        const int MAX_DEPTH = 10; // Change this value based on what's reasonable for your structures.
-
-        if (depth > MAX_DEPTH)
-        {
-            Debug.LogWarning($"Reached maximum depth at path: {parentPath}. Exiting...");
-            return;
-        }
-
-        if (obj == null) return;
-
-        if (obj is IDictionary)
-        {
-            IDictionary dictionary = obj as IDictionary;
-            foreach (DictionaryEntry entry in dictionary)
-            {
-                TestSerializabilityOfObject(entry.Value, $"{parentPath}/{entry.Key}", depth + 1);
-            }
-        }
-        else if (obj is IList)
-        {
-            IList list = obj as IList;
-            for (int i = 0; i < list.Count; i++)
-            {
-                TestSerializabilityOfObject(list[i], $"{parentPath}/[{i}]", depth + 1);
-            }
-        }
-        else if (obj is IEnumerable && !(obj is string))
-        {
-            // Handle other enumerable types like arrays or custom collections
-            int i = 0;
-            foreach (var item in (IEnumerable)obj)
-            {
-                TestSerializabilityOfObject(item, $"{parentPath}/[{i}]", depth + 1);
-                i++;
-            }
-        }
-
-        try
-        {
-            // Use the new SerializationUtility to get the JSON string.
-            string json = SerializationUtility.SerializeObject(obj);
-        }
-        catch (Exception ex)
-        {
-            Debug.LogError($"Serialization failed for path '{parentPath}' in type '{GetFriendlyTypeName(typeof(T))}'. Reason: {ex.Message}");
-        }
-    }
-
-
-
-
-    public static string GetFriendlyTypeName(Type t)
-    {
-        if (!t.IsGenericType)
-            return t.Name;
-
-        string name = t.Name.Substring(0, t.Name.IndexOf('`'));
-        string typeParameters = string.Join(", ", t.GetGenericArguments().Select(GetFriendlyTypeName).ToArray());
-        return $"{name}<{typeParameters}>";
-    }
-
-    public static Dictionary<string, Dictionary<string, List<string>>> ReadFromDisk(string filePath)
-    {
-        if (File.Exists(filePath))
-        {
-            string json = File.ReadAllText(filePath);
-            return JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, List<string>>>>(json);
-        }
-        else
-        {
-            Debug.LogError("File not found at: " + filePath);
-            return null;
-        }
-    }
+   
 
 
     /// <summary>
